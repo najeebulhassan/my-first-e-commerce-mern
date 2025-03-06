b30842f97155c96111782f1d4fa1bc9f0b536dc3
Muhammad Najeeb Ul Hassan <najeeb.hassan@ccjk.com>
2025-03-06 15:46:48 +0500

    static product getting displayed

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Nav.Link as={Link} to="/">
+            <Navbar.Brand>ProShop</Navbar.Brand>
+          </Nav.Link>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link as={Link} to="/cart">
+                <i className="fas fa-shopping-cart"></i>Cart
+              </Nav.Link>
+              <Nav.Link as={Link} to="/login">
+                <i className="fas fa-user"></i>Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
